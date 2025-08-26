b955f6c788838732524e78a4dfab13de292ab26a
CHHOTE-LAL-MEENA <chhotelalmeena372@gmail.com>
2025-08-26 18:57:52 +0530

    Initial Commit

---
+++ Star Patterns/10.hollowSquare.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void hollowSqaure(int n)
+{
+    int i, j;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            if (i == 1 || j == 1 || i == n || j == n)
+                cout << " * ";
+            else
+                cout << "   ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the no. of rows: ";
+    cin >> n;
+    hollowSqaure(n);
+    return 0;
+}
