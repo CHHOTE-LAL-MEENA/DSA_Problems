986414d568f4729c9db29ca644d91b7ea7101d53
CHHOTE-LAL-MEENA <chhotelalmeena372@gmail.com>
2025-08-27 22:16:22 +0530

    Initial commit

--- Star Patterns/21.ConcentricSquare.cpp
+++ Star Patterns/21.ConcentricSquare.cpp
@@ -8,7 +8,7 @@ void Arrow(int n)
     {
         for (j = 0; j < n; j++)
         {
-            if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+            if (i == 0 || j == 0 || j == n - 1 || i == n - 1)
                 cout << "*";
             else
                 cout << " ";
