1c4b302afc34e2d1be0f0c58b4f904e48445db24
CHHOTE-LAL-MEENA <chhotelalmeena372@gmail.com>
2025-09-12 12:04:53 +0530

    Initial Commit

--- Array/5.SearchElement.cpp
+++ Array/5.SearchElement.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void reverse(int arr[], int st, int end)
+int SearchEle(int arr[], int st, int end, int ele)  // log N time complexity
 {
-    while (st < end)
-    {
-        swap(arr[st++], arr[end--]);
-    }
-}
-
-int searchEle(int arr[], int st, int end, int k, int ele)
-{
-    reverse(arr, k, end);
-    reverse(arr, 0, k - 1);
-    reverse(arr, 0, end);
-
     while (st <= end)
     {
         int mid = st + (end - st) / 2;
         if (arr[mid] == ele)
             return mid;
-        if (arr[mid] < ele)
-            st = mid + 1;
+        if (arr[mid] >= arr[0])
+        {
+            if (arr[st] <= ele && arr[mid] > ele)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                st = mid + 1;
+            }
+        }
         else
-            end = mid - 1;
+        {
+            if (ele >= arr[mid] && ele < arr[end])
+            {
+                st = mid + 1;
+            }
+            else
+                end = mid - 1;
+        }
     }
     return -1;
 }
@@ -31,12 +35,8 @@ int searchEle(int arr[], int st, int end, int k, int ele)
 int main()
 {
     int arr[] = {2, 4, 6, 10, 1, 2};
-    int ele = 10, sz = 6, k = 4;
-    cout << "Element found at index: " << searchEle(arr, 0, sz - 1, k, ele) << endl;
-    for (int i = 0; i < sz; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    // cout<<searchEle(arr,1);
+    int ele = 4, sz = 6, k = 4;
+    cout << "Element found at index: " << SearchEle(arr, 0, sz - 1, ele);
+
     return 0;
 }
